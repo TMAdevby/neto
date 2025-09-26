6fe76e2a352556b7b0094b077ab78471a592e3b2
TMAdevby <mtipun@mail.ru>
2025-09-26 23:50:18 +0300

    lambdas

---
+++ src/main/java/org/example/Lambdas/first/Main.java
@@ -0,0 +1,37 @@
+package org.example.Lambdas.first;
+
+public class Main {
+    public static void main(String[] args) {
+        //Calc calc = new Calc(); через класс реализующий интерфейс
+
+        /*ISum summer = new ISum(){
+            @Override
+            public int sum(int a, int b) {
+                return a + b;
+            }
+        };*/ // через анонимный класс
+
+        ISum fn1 = (a,b) -> a+b;
+        ISum fn2 = (a,b) -> a-b;
+        ISum fn3 = (a,b) -> a*b;
+        ISum fn4 = (a,b) -> a/b;
+
+        System.out.println(fn1.fn(3,2));
+        System.out.println(fn2.fn(3,2));
+        System.out.println(fn3.fn(3,2));
+        System.out.println(fn4.fn(3,2));
+    }
+}
+
+@FunctionalInterface
+interface ISum{
+    int fn(int a,int b);
+}
+
+/*  через класс реализующий интерфейс
+class Calc implements ISum {
+    @Override
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}*/
---
+++ src/main/java/org/example/Lambdas/second/Main.java
@@ -0,0 +1,22 @@
+package org.example.Lambdas.second;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("Мадрид", "Париж", "Москва", "Токио");
+        /*Collections.sort(cities, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return b.compareTo(a);
+            }
+        });*/
+
+        Collections.sort(cities,(a, b) -> a.compareTo(b));
+
+        System.out.println(cities);
+    }
+}
