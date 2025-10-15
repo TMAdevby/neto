48e50772c70b5b3b480dc1228e4cf0078d8823c9
TMAdevby <mtipun@mail.ru>
2025-10-15 14:26:08 +0300

    JSON6

--- src/main/java/org/example/CSV_XML_JSON/JSON/Main2.java
+++ src/main/java/org/example/CSV_XML_JSON/JSON/Main2.java
@@ -2,19 +2,31 @@ package org.example.CSV_XML_JSON.JSON;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
 
 import java.awt.*;
 
 public class Main2 {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.name = "Матроскин";
-        cat.age = 5;
-        cat.color = Color.blue.getRGB();
+//        Cat cat = new Cat();
+//        cat.name = "Матроскин";
+//        cat.age = 5;
+//        cat.color = Color.blue.getRGB();
+//
+//        GsonBuilder builder = new GsonBuilder();
+//        builder.setPrettyPrinting();   //форматирует
+//        Gson gson = builder.create();
+//        System.out.println(gson.toJson(cat));
+
+        //gson deserialize
+        String jsonText = "{\"name\":\"Мурзик\",\"color\":-16777216,\"age\":9}";
 
         GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();   //форматирует
         Gson gson = builder.create();
-        System.out.println(gson.toJson(cat));
+        Cat cat = gson.fromJson(jsonText, Cat.class);
+        System.out.println("Имя: " + cat.name + "\nВозраст: " + cat.age + "\nЦвет: " +
+                cat.color);
+
+         builder.setPrettyPrinting().create();
     }
 }
