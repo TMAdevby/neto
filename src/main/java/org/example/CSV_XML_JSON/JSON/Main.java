f8d769f24d44ad87c9e5caf91b66c7c6bea55502
TMAdevby <mtipun@mail.ru>
2025-10-15 13:28:29 +0300

    JSON2

--- src/main/java/org/example/CSV_XML_JSON/JSON/Main.java
+++ src/main/java/org/example/CSV_XML_JSON/JSON/Main.java
@@ -1,6 +1,7 @@
 package org.example.CSV_XML_JSON.JSON;
 
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -27,6 +28,29 @@ public class Main {
 //        }
 
         // json simple sub object  доступ к полю объекта
+        JSONParser parser = new JSONParser();
+        try{
+            Object obj = parser.parse(new FileReader("data.json"));
+            JSONObject jsonObject = (JSONObject) obj;
 
+            String lastName = (String) jsonObject.get("lastName");
+            System.out.println(lastName);
+
+            JSONObject address = (JSONObject) jsonObject.get("address");
+            String city = (String) address.get("city");
+            System.out.println(city);
+
+            JSONArray phoneNumbers = (JSONArray) jsonObject.get("phoneNumbers");
+            for(Object number : phoneNumbers){
+                System.out.println(number);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
