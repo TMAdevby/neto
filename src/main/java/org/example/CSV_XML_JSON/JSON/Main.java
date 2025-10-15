635868c871c56a874cc33c9f0b93febe1442dd2d
TMAdevby <mtipun@mail.ru>
2025-10-15 13:34:08 +0300

    JSON4

--- src/main/java/org/example/CSV_XML_JSON/JSON/Main.java
+++ src/main/java/org/example/CSV_XML_JSON/JSON/Main.java
@@ -28,29 +28,31 @@ public class Main {
 //        }
 
         // json simple sub object  доступ к полю объекта
-        JSONParser parser = new JSONParser();
-        try{
-            Object obj = parser.parse(new FileReader("data.json"));
-            JSONObject jsonObject = (JSONObject) obj;
+//        JSONParser parser = new JSONParser();
+//        try{
+//            Object obj = parser.parse(new FileReader("data.json"));
+//            JSONObject jsonObject = (JSONObject) obj;
+//
+//            String lastName = (String) jsonObject.get("lastName");
+//            System.out.println(lastName);
+//
+//            JSONObject address = (JSONObject) jsonObject.get("address");
+//            String city = (String) address.get("city");
+//            System.out.println(city);
+//
+//            JSONArray phoneNumbers = (JSONArray) jsonObject.get("phoneNumbers");
+//            for(Object number : phoneNumbers){
+//                System.out.println(number);
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (ParseException e) {
+//            throw new RuntimeException(e);
+//        }
 
-            String lastName = (String) jsonObject.get("lastName");
-            System.out.println(lastName);
 
-            JSONObject address = (JSONObject) jsonObject.get("address");
-            String city = (String) address.get("city");
-            System.out.println(city);
-
-            JSONArray phoneNumbers = (JSONArray) jsonObject.get("phoneNumbers");
-            for(Object number : phoneNumbers){
-                System.out.println(number);
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
