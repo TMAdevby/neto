d3f39d349b5154373f313c7769c6b2b6aa2085be
TMAdevby <mtipun@mail.ru>
2025-10-17 20:22:05 +0300

    junit5

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,8 @@ import org.example.junitTesting.Calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -52,6 +54,19 @@ public class CalculatorTest {
         assertThatThrownBy(() -> calculator.divide(a,b))
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessage("Деление на ноль невозможно");
+    }
 
+    @ParameterizedTest(name = "{0} + {1} = {2} ")
+    @CsvSource({
+            "1,1,2",
+            "5,3,8",
+            "10,-4,6",
+            "0,0,0",
+            "-5,-3,-8"
+    })
+    @DisplayName("Сложение различных чисел")
+    void add_VariousNumber_ReturnCorrectSum(int a, int b, int expectedResult){
+        int result = calculator.add(a,b);
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
