710effdfed0d93cee6c739a93589316469870a59
TMAdevby <mtipun@mail.ru>
2025-10-19 19:04:36 +0300

    mock7

--- src/test/java/Mockito/AdviceServiceTest.java
+++ src/test/java/Mockito/AdviceServiceTest.java
@@ -9,12 +9,16 @@ class AdviceServiceTest {
     void test_get_advice_in_bad_weather() {
         WeatherServiceMock weatherService = new WeatherServiceMock();
         weatherService.setValue(Weather.STORMY);
+
         PreferencesServiceMock preferencesService = new PreferencesServiceMock();
         preferencesService.setValue(Set.of(Preference.FOOTBALL, Preference.WATCHING_FILMS,
                 Preference.READING));
+
         AdviceService adviceService = new AdviceService(preferencesService, weatherService);
+
         Set<Preference> preferences = adviceService.getAdvice("user1");
         Set<Preference> expected = Set.of(Preference.READING, Preference.WATCHING_FILMS);
+
         Assertions.assertEquals(expected, preferences);
     }
 }
