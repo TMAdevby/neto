3f8e629c5c9c121b4ec34b7a1ed9ffb3c819e985
TMAdevby <mtipun@mail.ru>
2025-10-20 19:45:37 +0300

    mock8

--- src/test/java/Mockito/AdviceServiceTest.java
+++ src/test/java/Mockito/AdviceServiceTest.java
@@ -4,6 +4,7 @@ import org.example.Mockito.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.Set;
+import org.mockito.Mockito;
 class AdviceServiceTest {
     @Test
     void test_get_advice_in_bad_weather() {
@@ -19,6 +20,22 @@ class AdviceServiceTest {
         Set<Preference> preferences = adviceService.getAdvice("user1");
         Set<Preference> expected = Set.of(Preference.READING, Preference.WATCHING_FILMS);
 
+        Assertions.assertEquals(expected, preferences);
+    }
+
+    @Test
+    void test_get_advice_in_bad_weather_mockito() {
+        WeatherService weatherService = Mockito.mock(WeatherService.class);
+        Mockito.when(weatherService.currentWeather())
+                .thenReturn(Weather.STORMY);
+
+        PreferencesService preferencesService = Mockito.mock(PreferencesService.class);
+        Mockito.when(preferencesService.get("user1"))
+                .thenReturn(Set.of(Preference.FOOTBALL, Preference.WATCHING_FILMS, Preference.READING));
+
+        AdviceService adviceService = new AdviceService(preferencesService, weatherService);
+        Set<Preference> preferences = adviceService.getAdvice("user1");
+        Set<Preference> expected = Set.of(Preference.READING, Preference.WATCHING_FILMS);
         Assertions.assertEquals(expected, preferences);
     }
 }
