748d0de1b87a73ad64ccf8124d58d0d889883d85
TMAdevby <mtipun@mail.ru>
2025-10-21 16:58:17 +0300

    mock12

--- src/test/java/Mockito/WeatherServiceImpl.java
+++ src/test/java/Mockito/WeatherServiceImpl.java
@@ -16,6 +16,11 @@ public class WeatherServiceImpl implements WeatherService {
     @Test
     void test_spy_weather_service() {
         WeatherService weatherService = Mockito.spy(WeatherServiceImpl.class);
+
+        Mockito.when(weatherService.currentWeather()).thenReturn(Weather.STORMY);
+
         Assertions.assertEquals(Weather.SUNNY, weatherService.currentWeather());
+
+        Mockito.verify(weatherService, Mockito.only()).currentWeather();
     }
 }
