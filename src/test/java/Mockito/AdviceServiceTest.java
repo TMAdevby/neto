3c14e6c7794b267139d824fd85931991326f222a
TMAdevby <mtipun@mail.ru>
2025-10-21 16:27:08 +0300

    mock11

--- src/test/java/Mockito/AdviceServiceTest.java
+++ src/test/java/Mockito/AdviceServiceTest.java
@@ -41,7 +41,7 @@ class AdviceServiceTest {
     }
 
     @Test
-    void test_get_advice_in_bad_weather_verify() {
+    void test_get_advice_in_bad_weather_mackito_argument_captor() {
         WeatherService weatherService = Mockito.mock(WeatherService.class);
         Mockito.when(weatherService.currentWeather()).thenReturn(Weather.STORMY);
 
@@ -51,8 +51,28 @@ class AdviceServiceTest {
         AdviceService adviceService = new AdviceService(preferencesService, weatherService);
         adviceService.getAdvice("user1");
 
+
         Mockito.verify(preferencesService, Mockito.times(1)).get("user1");
-        Mockito.verify(preferencesService, Mockito.times(0)).get("user2");
+        Mockito.verify(preferencesService, Mockito.times(1)).get("user2");
+        Mockito.verify(preferencesService, Mockito.times(2)).get(Mockito.anyString());
+    }
+
+    @Test
+    void test_get_advice_in_bad_weather_verify() {
+        WeatherService weatherService = Mockito.mock(WeatherService.class);
+        Mockito.when(weatherService.currentWeather()).thenReturn(Weather.STORMY);
+
+        PreferencesService preferencesService = Mockito.mock(PreferencesService.class);
+        Mockito.when(preferencesService.get(Mockito.any())).thenReturn(Set.of(Preference.FOOTBALL));
+
+        AdviceService adviceService = new AdviceService(preferencesService, weatherService);
+        adviceService.getAdvice("user1");
+        adviceService.getAdvice("user2");
+
+        Mockito.verify(weatherService, Mockito.times(2)).currentWeather();
+        Mockito.verify(preferencesService, Mockito.times(1)).get("user1");
+        Mockito.verify(preferencesService, Mockito.times(1)).get("user2");
+        Mockito.verify(preferencesService, Mockito.times(2)).get(Mockito.anyString());
     }
 }
 
