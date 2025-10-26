061ae4198719830def843e01d8068574c8702b78
TMAdevby <mtipun@mail.ru>
2025-10-26 17:02:51 +0300

    HTTP

---
+++ src/main/java/org/example/HTTP/Main.java
@@ -0,0 +1,37 @@
+package org.example.HTTP;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URI = "https://jsonplaceholder.typicode.com/posts";
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+// создание объекта запроса с произвольными заголовками
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+// отправка запроса
+        CloseableHttpResponse response = httpClient.execute(request);
+// вывод полученных заголовков
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+// чтение тела ответа
+        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+        System.out.println(body);
+    }
+}
