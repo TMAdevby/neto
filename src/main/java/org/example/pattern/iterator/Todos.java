2aa50e18195d20d988fbc3f33cde8010b90ae8e5
TMAdevby <mtipun@mail.ru>
2025-11-12 09:05:08 +0300

    pattern

---
+++ src/main/java/org/example/pattern/iterator/Todos.java
@@ -0,0 +1,18 @@
+package org.example.pattern.iterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Todos {
+
+    private List<String> primary = new ArrayList<>();
+    private List<String> secondary = new ArrayList<>();
+
+    public void addPrimary(String task){
+
+    }
+
+    public void addSecondary(String task){
+
+    }
+}
