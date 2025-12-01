fcaf497d5b98774c02fbff923f12e72520b8c6dd
TMAdevby <mtipun@mail.ru>
2025-12-01 19:51:00 +0300

    volatile2

---
+++ src/main/java/org/example/multithreading/synchCollection/Main.java
@@ -0,0 +1,12 @@
+package org.example.multithreading.synchCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+
+    }
+}
